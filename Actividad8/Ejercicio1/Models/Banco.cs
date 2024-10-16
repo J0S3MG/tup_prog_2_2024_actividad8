e1676d29844cac2645629a038337dfb83ba0cedf
J0S3MG <m0n0.gj81@gmail.com>
2024-10-16 15:44:05 -0300

    vs o.2

--- Actividad8/Ejercicio1/Models/Banco.cs
+++ Actividad8/Ejercicio1/Models/Banco.cs
@@ -1,13 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Ejercicio1.Models
 {
     public class Banco
     {
+        List<Persona> clientes = new List<Persona>();
+        public List<Cuenta> cuentas = new List<Cuenta>();
+
+        public int CantClien
+        {
+            get
+            {
+                return clientes.Count;
+            }
+        }
+        public int CantCuent
+        {
+            get
+            {
+                return cuentas.Count;
+            }
+        }
+        public Cuenta this[int idx]
+        {
+            get
+            {
+                Cuenta c = null;
+                if(idx > 0 && idx < CantCuent)
+                {
+                    c = cuentas[idx];
+                }
+                return c;
+            }
+        }
+
 
     }
 }
