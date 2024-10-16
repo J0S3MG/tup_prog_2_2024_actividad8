ca9af30d9e035b0618e08d247aaca43456f5d6a8
J0S3MG <m0n0.gj81@gmail.com>
2024-10-16 15:29:30 -0300

    Vs 0.1

---
+++ Actividad8/Ejercicio1/Models/Cuenta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio1.Models
+{
+    public class Cuenta: IComparable
+    {
+        public Persona Titular { get; set; }
+        public int Numero { get; set; }
+        public double Saldo { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public Cuenta(int num,Persona cliente)
+        {
+            Numero = num;
+            Fecha = DateTime.Today;
+            Saldo = 0;
+            Titular = cliente;
+        }
+        public Cuenta(int num, Persona cliente,double saldo,DateTime fecha)
+        {
+            Numero = num;
+            Fecha = fecha;
+            Saldo = saldo;
+            Titular = cliente;
+        }
+        public int CompareTo(Object obj)
+        {
+            Cuenta cuenta = obj as Cuenta;
+            if(cuenta != null) 
+                return Numero.CompareTo(cuenta.Numero);
+            return 1;
+        }
+    }
+}
---
+++ Actividad8/Ejercicio1/Models/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    public class Persona : IComparable
+    {
+        public int DNI { get; set; }
+        public string Nombre { get; set; }
+
+        public Persona(int dni, string nombre)
+        {
+            DNI = dni;
+            Nombre = nombre;
+        }
+        public int CompareTo(object obj)
+        {
+            Persona cliente = obj as Persona;
+            if (cliente != null) 
+                return DNI.CompareTo(cliente.DNI);
+            return 1;
+        }
+    }
+}
